1aee5839fca70112f3a1a2a00a79787c95f70c29
lhzbxx <lhzbxx@gmail.com>
2016-07-16 03:29:22 +0800

    Version 1.0

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import random
 import string
+import os
 
 from flask import *
 app = Flask(__name__)
 
-app.secret_key = 'lhzbxx'
+app.secret_key = os.urandom(32)
 
-card = {
+card_type = {
     "侍卫": "说出一个非侍卫牌名称并选择一名玩家，若此玩家拥有此牌则被淘汰",
     "牧师": "查看另一名玩家的手牌",
     "男爵": "你和另一名玩家比较手牌点数，点数较小的玩家将被淘汰。",
@@ -36,6 +37,8 @@ def before_request():
         return "Not authorized!"
     if request.endpoint in progress_endpoint and request.args.get('room') == '':
         return "Room not exists!"
+    if request.endpoint in progress_endpoint and not session['player_token:' + request.args.get('player')]:
+        return "Invalid token!"
     if not session.get('room'):
         session['room'] = []
 
@@ -49,9 +52,11 @@ def random_str(length=8):
 @app.route('/join/<room>/<player>')
 def join(room, player):
     player_token = random_str()
-    init_player(player_token, player)
     if room not in session['room']:
         init_room(room)
+    if player in session['room:' + room + ':player']:
+        return "Duplicate player!"
+    init_player(player_token, player)
     if not is_room_full(room):
         player_join_room(room, player)
     return player_token
@@ -83,6 +88,14 @@ def ready():
     session['room:' + room + ':player:' + player + ':status'] = True
     if len(session['room:' + room + ':player']) > 1 and is_all_player_ready(room):
         game_start(room)
+    return jsonify([{i: session['room:' + room + ':player:' + i + ':status']}
+                    for i in session['room:' + room + ':player']])
+
+
+@app.route('/all')
+def display_all():
+    print(session)
+    return "OK"
 
 
 def is_all_player_ready(room):
@@ -94,8 +107,9 @@ def is_all_player_ready(room):
 
 def game_start(room):
     for i in session['room:' + room + ':player']:
-        session['room:' + room + ':player:' + i + ':deck'] = []
+        session['room:' + room + ':player:' + i + ':hand'] = []
     session['room:' + room + ':deck'] = deck_template
+    session['room:' + room + ':fold_deck'] = []
 
 
 def game_end(room):
@@ -110,24 +124,58 @@ def show_room(room):
 
 @app.route('/draw')
 def draw():
+    room = request.args.get('room')
+    player = session['player_token:' + request.args.get('player')]
+    player_add_one_card(room, player, draw_one_card_from_deck(room))
     return 'draw'
+
+
+def draw_one_card_from_deck(room):
+    index = random.randint(0, len(session['room:' + room + ':deck']) - 1)
+    return session['room:' + room + ':deck'].pop(index)
+
+
+def player_add_one_card(room, player, card):
+    session['room:' + room + ':player:' + player + ':hand'].append(card)
 
 
 @app.route('/fold')
 def fold():
-    return "fold"
+    room = request.args.get('room')
+    player = session['player_token:' + request.args.get('player')]
+    if not is_player_fold_or_use_card_valid(room, player):
+        return 'Invalid card!'
+    card_fold_or_use(room, player, request.args.get('card'))
+    return redirect(url_for(show, room=room, player=request.args.get('player')))
 
 
 @app.route('/use')
 def use():
-    return 'use'
+    room = request.args.get('room')
+    player = session['player_token:' + request.args.get('player')]
+    if not is_player_fold_or_use_card_valid(room, player):
+        return 'Invalid card!'
+    # todo: card using engine to handle!
+    card_fold_or_use(room, player, request.args.get('card'))
+    return redirect(url_for(show, room=room, player=request.args.get('player')))
+
+
+def is_player_fold_or_use_card_valid(room, player):
+    if not request.args.get('card'):
+        return False
+    return request.args.get('card') in session['room:' + room + ':player:' + player + ':hand']
+
+
+def card_fold_or_use(room, player, card):
+    request.args.get('card') in session['room:' + room + ':player:' + player + ':hand'].remove(card)
+    session['room:' + room + ':fold_deck'].append(card)
 
 
 @app.route('/show')
 def show():
     room = request.args.get('room')
     player = session['player_token:' + request.args.get('player')]
-    return session['room:' + room + ':player:' + player + ':hand']
+    return jsonify(session['room:' + room + ':player:' + player + ':hand'])
 
 
 @app.route('/')
