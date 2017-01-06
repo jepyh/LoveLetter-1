b6e1e4a3a5895438d415aba03435ef44a37e79a5
lhzbxx <lhzbxx@gmail.com>
2017-01-06 10:38:59 +0800

    Add store.

--- service/src/index.js
+++ service/src/index.js
@@ -1,12 +1,12 @@
 import socket from 'socket.io'
 import CONFIG from './config'
+import store from './store'
 
 const io = socket()
 
 io.on('connection', (socket) => {
   let socketId = socket.client.conn.id
   console.log('user ' + socketId + ' connected')
-  console.log(CONFIG.CONSTANTS.DECK)
   socket.on('disconnect', () => {
     console.log('user ' + socketId + ' disconnected')
   })
@@ -23,5 +23,10 @@ const deal = () => {
 }
 
 console.log('server start: 3000')
+
+console.log(CONFIG.CONSTANTS.DECK)
+
+console.log(store.room)
+console.log(store.user)
 
 io.listen(3000)
---
+++ service/src/store/index.js
@@ -0,0 +1,7 @@
+import room from './module/rooms'
+import user from './module/users'
+
+export default {
+  room,
+  user
+}
