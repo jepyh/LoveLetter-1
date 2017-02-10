3a8e522539e95c7dcd3545c493ca4b69d3ff5b36
lhzbxx <lhzbxx@gmail.com>
2017-02-10 20:02:52 +0800

    .

--- mobile/config/dev.env.js
+++ mobile/config/dev.env.js
@@ -3,5 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  URL: '"http://192.168.11.150:10016"'
+  // URL: '"http://192.168.11.150:10016"'
+  URL: '"http://localhost:10016"'
 })
